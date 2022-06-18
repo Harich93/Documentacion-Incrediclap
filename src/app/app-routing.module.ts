f9808014b3fac43e89ae861a707c9b8ce6228c83
Beni López <blopmar93@gmail.com>
2022-06-18 19:40:47 +0200

    build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManualUsuarioComponent } from './pages/manual-usuario/manual-usuario.component';
 import { DocumentacionFrontendComponent } from './pages/documentacion-frontend/documentacion-frontend.component';
+import { ChangePassComponent } from './pages/change-pass/change-pass.component';
 
 const routes: Routes = [
   { path: '', component: ManualUsuarioComponent }, 
   { path: 'manual-usuario', component: ManualUsuarioComponent },
   { path: 'doc-front', component: DocumentacionFrontendComponent },
+  { path: 'change-pass', component: ChangePassComponent },
 ];
 
 @NgModule({
